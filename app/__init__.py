7c05268e77462b28389330d84bbb740dd5bcda1d
Elisa <113119301+CheesecakeTM@users.noreply.github.com>
2025-10-13 16:16:28 +0200

    Aggiornato il Procfile per utilizzare run:app e ristrutturato l'inizializzazione dell'app Flask in run.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,38 +5,40 @@ from .models.models import db, Admin
 from .utils import create_admin_account
 from config import config
 
+app = Flask(__name__)
 
-def create_app(app):
+admin_username = config.ADMIN_USER
+admin_password = config.ADMIN_PASS
 
-    admin_username = config.ADMIN_USER
-    admin_password = config.ADMIN_PASS
+app.secret_key = config.SECRET_KEY
 
-    app.secret_key = config.SECRET_KEY
+BASE_DIR = Path(__file__).resolve().parent
+DB_DIR = BASE_DIR / "db"
+DB_DIR.mkdir(exist_ok=True)
+db_path = DB_DIR / "attivita_ufo.db"
 
-    BASE_DIR = Path(__file__).resolve().parent
-    DB_DIR = BASE_DIR / "db"
-    DB_DIR.mkdir(exist_ok=True)
-    db_path = DB_DIR / "attivita_ufo.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_path}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_path}"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
 
-    db.init_app(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
-    login_manager = LoginManager()
-    login_manager.init_app(app)
+setattr(login_manager, "login_view", "admin.login")
 
-    setattr(login_manager, "login_view", "admin.login")
 
-    @login_manager.user_loader
-    def load_user(admin_id):
-        return Admin.query.get(int(admin_id))
+@login_manager.user_loader
+def load_user(admin_id):
+    return Admin.query.get(int(admin_id))
 
-    from .routes import main_bp, admin_bp
 
-    app.register_blueprint(main_bp)
-    app.register_blueprint(admin_bp, url_prefix="/admin")
+from .routes.main import main_bp
+from .routes.admin import admin_bp
 
-    with app.app_context():
-        db.create_all()
-        create_admin_account(admin_username, admin_password)
+app.register_blueprint(main_bp)
+app.register_blueprint(admin_bp, url_prefix="/admin")
+
+with app.app_context():
+    db.create_all()
+    create_admin_account(admin_username, admin_password)
--- run.py
+++ run.py
@@ -1,13 +1,9 @@
 import os
 from flask import Flask
-from app import create_app
+from app import app
 from config import config
 
 
-app = Flask(__name__, template_folder="app/templates", static_folder="app/static")
-
-
 if __name__ == "__main__":
-    create_app(app)
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=config.DEBUG)
